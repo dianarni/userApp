ca11cf0b81cc1f20526a71a44ed659c848cc201e
dianarni <142962375+dianarni@users.noreply.github.com>
2023-10-03 11:02:37 +0700

    Add files via upload

---
+++ main/java/com/ui/UsersNameAdapter.kt
@@ -0,0 +1,50 @@
+package com.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.data.response.GithubResponse
+import com.data.response.ItemsItem
+import com.example.githubuserapp.databinding.ItemUserListBinding
+
+class UsersNameAdapter : ListAdapter<ItemsItem, UsersNameAdapter.MyViewHolder>(DIFF_CALLBACK) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val binding = ItemUserListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MyViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val user = getItem(position)
+        holder.bind(user)
+    }
+
+    class MyViewHolder (val binding: ItemUserListBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(user: ItemsItem) {
+            binding.apply {
+                Glide.with(itemView)
+                    .load(user.avatarUrl)
+                    .transition(DrawableTransitionOptions.withCrossFade())
+                    .centerCrop()
+                    .into(tvPhotoProfile)
+                tvUserName.text = user.login
+            }
+        }
+    }
+
+    companion object {
+        val DIFF_CALLBACK = object : DiffUtil.ItemCallback<ItemsItem>() {
+            override fun areItemsTheSame(oldItem: ItemsItem, newItem: ItemsItem): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(oldItem: ItemsItem, newItem: ItemsItem): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
